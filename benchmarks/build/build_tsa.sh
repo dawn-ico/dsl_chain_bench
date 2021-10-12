a94c4c1c1748d590f3e46ba942296696677c2e84
mroethlin <Matthias.Roethlin@MeteoSwiss.ch>
2021-10-12 14:27:25 +0200

    change atlas utils path

--- benchmarks/build/build_tsa.sh
+++ benchmarks/build/build_tsa.sh
@@ -7,8 +7,8 @@ module use /apps/common/UES/sandbox/kraushm/tsa-nvhpc/easybuild/modules/all;
 module load nvhpc
 
 cmake .. -Datlas_DIR=/scratch/mroeth/spack-install/tsa/atlas/0.22.0/gcc/nv64bckunjhyj2nf6vny22hd5flrc3gj/lib64/cmake/atlas/ \
-         -Datlas_utils_DIR=/project/g110/spack-install/tsa/atlas_utilities/master/gcc/no3ecxk2xcgd2wgowopzrb2miwyk367z/lib/cmake/atlas_utils \
-         -Datlas_utils_LIBRARY_PATH=/project/g110/spack-install/tsa/atlas_utilities/master/gcc/no3ecxk2xcgd2wgowopzrb2miwyk367z/lib/libatlasUtilsLib.a \
+         -Datlas_utils_DIR=/scratch/mroeth/spack-install/tsa/atlas_utilities/master/gcc/no3ecxk2xcgd2wgowopzrb2miwyk367z/lib/cmake/atlas_utils \
+         -Datlas_utils_LIBRARY_PATH=/scratch/mroeth/spack-install/tsa/atlas_utilities/master/gcc/no3ecxk2xcgd2wgowopzrb2miwyk367z/lib/libatlasUtilsLib.a \
          -Ddawn4py_DIR=/scratch/mroeth/dawn/dawn/src/ \
          -DTOOLCHAINPATH=/scratch/mroeth/dawn/build/install/bin:/scratch/mroeth/dusk-venv/bin \
          -DCMAKE_BUILD_TYPE=Release
