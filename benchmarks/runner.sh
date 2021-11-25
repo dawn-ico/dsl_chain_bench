82e288311b9dc80f509b05ea6629ceb77533f5df
mroethlin <Matthias.Roethlin@MeteoSwiss.ch>
2021-11-25 14:34:29 +0100

    make runner a little more convenient

--- benchmarks/runner.sh
+++ benchmarks/runner.sh
@@ -5,6 +5,7 @@ pushd build
   for bench in $benches 
   do
     echo "srun --partition debug --gres=gpu:1 $bench"
-    srun --partition debug --gres=gpu:1 $bench
+    rt=$(srun --partition debug --gres=gpu:1 $bench)
+    echo $rt >> default_times.txt
   done 
 popd build
